2e07188448bbe496473c20aa57119df99ce1f9ae
Frederic Heem <frederic.heem@gmail.com>
2023-06-07 13:08:45 +0100

    benchmark

---
+++ bau/README.md
@@ -0,0 +1,13 @@
+# Bau.js
+
+A Javascript library to write user interface for the web under 1.5KB.
+
+No JSX, no templating, just a simple way to write UI components using Javascript.
+
+## Workflow
+
+Install the dependencies:
+
+```sh
+npm install @grucloud/bau
+```
---
+++ examples/benchmark/main.js
@@ -0,0 +1,239 @@
+import Ban from "@grucloud/bau";
+
+const ban = Ban();
+
+const { a, button, div, tr, td, table, tbody, h1, span } = ban.tags;
+
+const random = (max) => Math.round(Math.random() * 1000) % max;
+
+const A = [
+  "pretty",
+  "large",
+  "big",
+  "small",
+  "tall",
+  "short",
+  "long",
+  "handsome",
+  "plain",
+  "quaint",
+  "clean",
+  "elegant",
+  "easy",
+  "angry",
+  "crazy",
+  "helpful",
+  "mushy",
+  "odd",
+  "unsightly",
+  "adorable",
+  "important",
+  "inexpensive",
+  "cheap",
+  "expensive",
+  "fancy",
+];
+const C = [
+  "red",
+  "yellow",
+  "blue",
+  "green",
+  "pink",
+  "brown",
+  "purple",
+  "brown",
+  "white",
+  "black",
+  "orange",
+];
+const N = [
+  "table",
+  "chair",
+  "house",
+  "bbq",
+  "desk",
+  "car",
+  "pony",
+  "cookie",
+  "sandwich",
+  "burger",
+  "pizza",
+  "mouse",
+  "keyboard",
+];
+
+let nextId = 1;
+
+const buildData = (count) => {
+  const data = new Array(count);
+
+  for (let i = 0; i < count; i++) {
+    data[i] = {
+      id: nextId++,
+      label: `${A[random(A.length)]} ${C[random(C.length)]} ${
+        N[random(N.length)]
+      }`,
+    };
+  }
+
+  return data;
+};
+
+const dataState = bau.state([]);
+const selectedState = bau.state(0);
+
+const run = () => {
+  dataState.val = buildData(1000);
+  selectedState.val = 0;
+};
+
+const runLots = () => {
+  dataState.val = buildData(10000);
+  selectedState.val = 0;
+};
+
+const add = () => {
+  dataState.val = [...dataState.val, ...buildData(1000)];
+};
+
+const update = () => {
+  for (let i = 0; i < dataState.val.length; i += 10) {
+    const r = dataState.val[i];
+    dataState.val[i] = { id: r.id, label: r.label + " !!!" };
+  }
+};
+
+const swapRows = () => {
+  if (dataState.val.length > 998) {
+    const data = dataState.val;
+    const dataTmp = data[1];
+    dataState.val[1] = data[998];
+    dataState.val[998] = dataTmp;
+  }
+};
+
+const clear = () => {
+  dataState.val = [];
+  selectedState.val = 0;
+};
+
+const remove = ({ id, event }) => {
+  event.preventDefault();
+  const idx = dataState.val.findIndex((d) => d.id === id);
+  if (idx > -1) {
+    dataState.val.splice(idx, 1);
+  }
+};
+
+const select = ({ id, event }) => {
+  event.preventDefault();
+  selectedState.val = id;
+};
+
+const Row = ({ id, label }) =>
+  tr(
+    {
+      class: {
+        deps: [selectedState],
+        f: (selected) => (selected == id ? "danger" : ""),
+      },
+    },
+
+    td({ class: "col-md-1" }, id),
+    td(
+      { class: "col-md-4" },
+      a({ href: "", onclick: (event) => select({ id, event }) }, label)
+    ),
+    td(
+      { class: "col-md-1" },
+      a(
+        { onclick: (event) => remove({ id, event }) },
+        span(
+          { class: "glyphicon glyphicon-remove", "aria-hidden": true },
+          "delete"
+        )
+      )
+    ),
+    td({ class: "col-md-6" })
+  );
+
+const Button = ({ id, title, onclick }) =>
+  div(
+    { class: "col-sm-6 smallpad" },
+    button(
+      { type: "button", class: "btn btn-primary btn-block", id, onclick },
+      title
+    )
+  );
+
+const Jumbotron = ({}) =>
+  div(
+    { class: "jumbotron" },
+    div(
+      { class: "row" },
+      div({ class: "col-md-6" }, h1("Bau")),
+      div(
+        { class: "col-md-6" },
+        div(
+          { class: "row" },
+          Button({ id: "run", title: "Create 1,000 rows", onclick: run }),
+          Button({
+            id: "runlots",
+            title: "Create 10,000 rows",
+            onclick: runLots,
+          }),
+          Button({
+            id: "add",
+            title: "Append 1,000 rows",
+            onclick: add,
+          }),
+          Button({
+            id: "update",
+            title: "Update every 10th row",
+            onclick: update,
+          }),
+          Button({
+            id: "clear",
+            title: "Clear",
+            onclick: clear,
+          }),
+          Button({
+            id: "swaprows",
+            title: "Swap Row",
+            onclick: swapRows,
+          })
+        )
+      )
+    )
+  );
+
+const Main = () =>
+  div(
+    { class: "container" },
+    Jumbotron({}),
+    table(
+      { class: "table table-hover table-striped test-data" },
+      bau.bind({
+        deps: [dataState, selectedState],
+        render:
+          ({ renderItem }) =>
+          (arr) => {
+            return tbody(arr.map(renderItem()));
+          },
+        renderItem: () => (value) => {
+          return Row({
+            id: value.id,
+            item: value,
+            label: value.label,
+          });
+        },
+      }),
+      span({
+        class: "preloadicon glyphicon glyphicon-remove",
+        "aria-hidden": true,
+      })
+    )
+  );
+
+const app = document.getElementById("app");
+app.replaceChildren(Main({}));
