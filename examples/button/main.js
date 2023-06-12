2c1599a70dac6ed04e741564a27a6531800b8141
Frederic Heem <frederic.heem@gmail.com>
2023-06-12 16:28:02 +0100

    add button example

---
+++ examples/button/main.js
@@ -0,0 +1,50 @@
+import "./style.css";
+//import Bau from "@grucloud/bau";
+import Bau from "../../bau/src/bau";
+
+const app = ({ bau }) => {
+  const { h3, button, div } = bau.tags;
+
+  const Button = ({ color, onclick }, ...children) =>
+    button(
+      {
+        style: { deps: [color], f: (color) => `background-color: ${color};` },
+        onclick,
+      },
+      ...children
+    );
+
+  const colorState = bau.state("green");
+  const textState = bau.state("Turn Red");
+
+  const turnRed = () => {
+    colorState.val = "red";
+    textState.val = "Turn Green";
+    onclickState.val = turnGreen;
+  };
+
+  const turnGreen = () => {
+    colorState.val = "green";
+    textState.val = "Turn Red";
+    onclickState.val = turnRed;
+  };
+
+  const onclickState = bau.state(turnRed);
+
+  return function Main() {
+    return div(
+      h3("Buttton with primitive props"),
+      Button(
+        {
+          color: "yellow",
+          onclick: () => alert("Clicked"),
+        },
+        "Click Me"
+      ),
+      h3("Buttton with state props"),
+      Button({ color: colorState, onclick: onclickState }, textState)
+    );
+  };
+};
+
+document.getElementById("app").replaceChildren(app({ bau: Bau() })());
