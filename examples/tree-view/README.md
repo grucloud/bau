3ab818b763be718506574830829f824f9c7d2244
Frederic Heem <frederic.heem@gmail.com>
2023-06-17 22:16:23 +0200

    tree view

---
+++ examples/tree-view/README.md
@@ -0,0 +1,23 @@
+# Tree View
+
+This example shows how to implement a tree view to display nested data.
+
+## Workflow
+
+Install the dependencies:
+
+```sh
+npm install
+```
+
+Start a development server:
+
+```sh
+npm run dev
+```
+
+Build a production version:
+
+```sh
+npm run build
+```
---
+++ examples/tree-view/main.js
@@ -0,0 +1,86 @@
+//import Bau from "@grucloud/bau";
+import Bau from "../../bau/src/bau";
+
+const bau = Bau();
+const { div, h1, b, a, span } = bau.tags;
+
+const isObject = (value) => typeof value == "object";
+
+const myData = {
+  name: "John Doe",
+  age: 30,
+  email: "johndoe@example.com",
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345",
+  },
+  phone_numbers: [
+    { type: "home", number: "555-1234" },
+    { type: "work", number: "555-5678" },
+  ],
+};
+
+const Tree = ({ depth = 0 }) => {
+  const Key = ({ key, value, hideState }) => {
+    const keyEl = key !== "" ? `${key}: ` : "";
+    const prefixIcon = bau.bind({
+      deps: [hideState],
+      render: () => (hide) => hide ? "➕ " : "➖ ",
+    });
+    const postIcon = bau.bind({
+      deps: [hideState],
+      render: () => (hide) => hide ? "…" : "",
+    });
+
+    return Array.isArray(value)
+      ? a(
+          {
+            style: "cursor: pointer",
+            onclick: () => (hideState.val = !hideState.val),
+          },
+          b(prefixIcon, keyEl, postIcon)
+        )
+      : b("🟰 ", keyEl);
+  };
+
+  const Value = ({ value, hideState }) =>
+    isObject(value)
+      ? div(
+          {
+            style: {
+              deps: [hideState],
+              renderProp: (hide) => (hide ? "display: none" : ""),
+            },
+          },
+          value
+        )
+      : value;
+
+  const TreeItem = ({ key = "", value, depth = 0 }) => {
+    const style = `padding-left: ${depth > 0 ? 1 : 0}rem;`;
+    const hideState = bau.state(depth > 0);
+    return div(
+      { style },
+      Key({
+        key,
+        value,
+        hideState,
+      }),
+      Value({ value, hideState })
+    );
+  };
+
+  return ([key, value]) =>
+    TreeItem({
+      key,
+      value: isObject(value)
+        ? Object.entries(value).map(Tree({ depth: depth + 1 }))
+        : value,
+      depth,
+    });
+};
+const App = () => div(h1("Tree View example"), Tree({})(["", myData]));
+
+document.getElementById("app").replaceChildren(App({}));
