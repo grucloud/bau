51bfcfbd7bb912e0d4258b31ba927a33322abc0f
Frederic Heem <frederic.heem@gmail.com>
2023-07-17 16:45:13 +0200

    treeview in navbar

--- bau-ui/examples/bau-storybook/src/navBarMenu.js
+++ bau-ui/examples/bau-storybook/src/navBarMenu.js
@@ -1,8 +1,40 @@
+import treeView from "@grucloud/bau-ui/treeView";
+
+import { componentList } from "./componentListData";
+import { pagesList } from "./pagesListData";
+
 export default function (context) {
   const { tr, bau, css } = context;
-  const { ul, li, nav, a } = bau.tags;
+  const { ul, li, nav, a, span } = bau.tags;
 
-  return function NavBarMenu({ componentList }) {
+  const tree = {
+    data: { name: "Root Menu" },
+    children: [
+      {
+        data: { name: "Components" },
+        expanded: true,
+        children: componentList().map((data) => ({ data })),
+      },
+      {
+        data: { name: "Pages", id: "/pages" },
+        expanded: true,
+        children: pagesList().map((data) => ({ data })),
+      },
+    ],
+  };
+
+  const renderMenuItem = ({ name, id }) =>
+    (id ? a : span)(
+      {
+        href: `#${id}`,
+        onclick: (event) => {},
+      },
+      name
+    );
+
+  const TreeView = treeView(context, { renderMenuItem });
+
+  return function NavBarMenu({}) {
     return nav(
       {
         class: css`
@@ -10,49 +42,11 @@ export default function (context) {
           position: sticky;
           top: calc(var(--header-height) + 1rem);
           align-self: start;
-          bottom: 0px;
-          left: 0;
-          min-width: 150px;
-          max-width: 180px;
           overflow-y: scroll;
-          height: max-content;
-          box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.5);
-          max-height: 95vh;
-          & ul {
-            padding: 0;
-            > li {
-              cursor: pointer;
-              margin: 10px 10px;
-              list-style: none;
-              > a {
-                margin: 20px 0px;
-                text-transform: uppercase;
-                text-decoration-line: none;
-                letter-spacing: 0.1em;
-                font-weight: bold;
-                color: var(--font-color-base);
-                &:hover {
-                  color: var(--color-primary);
-                  &::after {
-                    background-color: var(--color-primary);
-                    transform: translate(0%);
-                  }
-                }
-                &::after {
-                  content: "";
-                  display: block;
-                  margin-top: 6px;
-                  height: 3px;
-                  transition: 0.2s ease-out;
-                  transform: translate(-100%);
-                  transform-origin: right;
-                }
-              }
-            }
-          }
+          height: calc(100vh - var(--header-height) - 1rem);
         `,
       },
-      ul(componentList.map(({ name, id }) => li(a({ href: `#${id}` }, name))))
+      TreeView({ tree })
     );
   };
 }
