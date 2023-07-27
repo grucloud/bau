6610e08653360aaaade24099adca494d3e12705b
Frederic Heem <frederic.heem@gmail.com>
2023-07-27 09:43:24 -0300

    fix findSubTree

--- bau-ui/drillDownMenu/drillDownMenu.js
+++ bau-ui/drillDownMenu/drillDownMenu.js
@@ -218,7 +218,7 @@ export default function (context, { renderMenuItem }) {
 
     navEl.append(
       Menu({
-        currentTree,
+        currentTree: subTree,
         onclickItem,
         onclickBack,
       })
