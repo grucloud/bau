255f26c9b392793fa2ae71486027f1a2f17a8064
Frederic Heem <frederic.heem@gmail.com>
2023-08-05 16:14:27 -0300

    Updates

--- bausaurus/assets/navBarTree-56abcb85.js
+++
@@ -1,4 +0,0 @@
-const navBarTree = { "children": [{ "data": { "name": "Introduction", "href": "/bau/bausaurus/docs/index" }, "index": true }, { "data": { "name": "Installation", "href": "/bau/bausaurus/docs/Installation" } }, { "data": { "name": "Commands", "href": "/bau/bausaurus/docs/Commands" } }, { "data": { "name": "Configuration", "href": "/bau/bausaurus/docs/Configuration" } }, { "data": { "name": "Markdown", "href": "/bau/bausaurus/docs/Markdown" } }] };
-export {
-  navBarTree
-};
