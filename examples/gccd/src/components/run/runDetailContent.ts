971cbbca3bbb630c9bf79b4b03dc5e959a526cf8
Frederic Heem <frederic.heem@gmail.com>
2023-09-27 15:19:18 -0300

    run web socket WIP

--- examples/gccd/src/components/run/runDetailContent.ts
+++ examples/gccd/src/components/run/runDetailContent.ts
@@ -3,7 +3,7 @@ import tableContainer from "@grucloud/bau-ui/tableContainer";
 
 export default function (context: Context) {
   const { bau, css } = context;
-  const { h2, table, tr, td, th, section } = bau.tags;
+  const { h2, table, tr, td, th, section, a } = bau.tags;
   const TableContainer = tableContainer(context, {
     class: css`
       & th {
@@ -11,10 +11,59 @@ export default function (context: Context) {
       }
     `,
   });
-  return function RunDetailContent({ run_id }: any) {
+  return function RunDetailContent({
+    org_id,
+    workspace_id,
+    project_id,
+    run_id,
+    container_id,
+    status,
+    logsUrl,
+  }: any) {
+    // console.log(
+    //   "container_id",
+    //   org_id,
+    //   project_id,
+    //   workspace_id,
+    //   run_id,
+    //   container_id,
+    //   status
+    // );
+    if (status == "creating") {
+      // TODO window.location.host ?
+      const socket = new WebSocket("ws://localhost:9000");
+      // Connection opened
+      socket.addEventListener("open", (event) => {
+        console.log("open");
+        socket.send(
+          JSON.stringify({
+            command: "Run",
+            options: { org_id, project_id, workspace_id, run_id, container_id },
+          })
+        );
+      });
+      socket.addEventListener("close", (event) => {
+        console.log("websocket closed", event);
+      });
+      socket.addEventListener("error", (event) => {
+        console.log("websocket error");
+      });
+      socket.addEventListener("message", (event) => {
+        console.log("Message from server ", event.data);
+      });
+    }
     return section(
       h2("Summary"),
-      TableContainer(table(tr(th("Run Id"), td(run_id))))
+      TableContainer(
+        table(
+          tr(th("Run Id"), td(run_id)),
+          tr(th("Status"), td(status)),
+          tr(
+            th("Logs"),
+            td(a({ href: logsUrl, target: "_blank" }, "Download log file"))
+          )
+        )
+      )
     );
   };
 }
