dd9b54ae1d8c3a6e16abe3a0648d6386784a7f13
Frederic Heem <frederic.heem@gmail.com>
2023-10-14 11:14:56 -0300

    initial tabs from location hash

--- bau-ui/examples/bau-storybook/src/pages/tabs/tabs-example-default.ts
+++ bau-ui/examples/bau-storybook/src/pages/tabs/tabs-example-default.ts
@@ -3,22 +3,22 @@ import { Context } from "@grucloud/bau-ui/context";
 
 export default (context: Context) => {
   const { bau } = context;
-  const { div, p } = bau.tags;
+  const { div, p, a } = bau.tags;
 
   const tabDefs: Tabs = [
     {
       name: "Tab1",
-      Header: () => div("TAB"),
+      Header: () => a({ href: "#Tab1" }, "TAB"),
       Content: () => div(p("My Tab 1 Content")),
     },
     {
       name: "Tab2",
-      Header: () => div("TAB 2"),
+      Header: () => a({ href: "#Tab2" }, "TAB 2"),
       Content: () => div(p("My tab 2 Content")),
     },
   ];
 
-  const Tabs = tabs(context, { tabDefs });
+  const Tabs = tabs(context, { tabDefs, variant: "outline", color: "neutral" });
 
-  return () => Tabs({ variant: "outline", color: "neutral" });
+  return () => Tabs({});
 };
