2af8ae174c742475df562b3c88f5f7818367fb16
Frederic Heem <frederic.heem@gmail.com>
2023-11-13 14:22:15 -0300

    fix bausaurus/example/bausaurus-template

--- bausaurus/example/bausaurus-template/bausaurus.config.js
+++ bausaurus/example/bausaurus-template/bausaurus.config.js
@@ -6,7 +6,8 @@ export default ({ rootDir }) => {
     site: {
       rootDir,
       favicon: "/grucloud.svg",
-      base: "/docs/",
+      //base: "/docs/",
+      baseDoc: "docs/",
       outDir: "dist/docs",
       srcDir: "docs",
       title: "Your title",
