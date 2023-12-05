c318985b7dc4e411395456243ea85bf5f8419297
Frederic Heem <frederic.heem@gmail.com>
2023-12-05 15:40:40 -0300

    add NoOrg

--- examples/gccd/src/components/org/orgList.ts
+++ examples/gccd/src/components/org/orgList.ts
@@ -1,15 +1,20 @@
 import { type Context } from "@grucloud/bau-ui/context";
 import tableContainer from "@grucloud/bau-ui/tableContainer";
 import tableSkeleton from "../tableSkeleton";
+import alert from "@grucloud/bau-ui/alert";
+import button from "@grucloud/bau-ui/button";
 
 export default function (context: Context) {
-  const { bau, css } = context;
-  const { span, a, table, thead, tbody, th, tr, td } = bau.tags;
-
+  const { bau, css, config } = context;
+  const { h1, p, span, a, table, thead, tbody, tfooter, th, tr, td } = bau.tags;
+  const Alert = alert(context, {
+    color: "primary",
+  });
   const TableSkeleton = tableSkeleton(context);
   const TableContainer = tableContainer(context, {
     class: css``,
   });
+  const ButtonAdd = button(context, { color: "primary", variant: "solid" });
 
   const ListItem = ({ org_id, project_count }: any) =>
     tr(
@@ -27,14 +32,41 @@ export default function (context: Context) {
       td(project_count)
     );
 
+  const NoOrg = () =>
+    tfooter(
+      Alert(
+        {
+          class: css`
+            margin: 1rem;
+          `,
+        },
+        h1("No organisation"),
+        p("An organisation is a container for projects."),
+        p(
+          ButtonAdd(
+            {
+              href: `${config.base}/org/create`,
+            },
+            "Create Organisation"
+          )
+        )
+      )
+    );
+
   const headers = ["Organisation", "Projects"];
 
   return function OrgList({ data, loading }: any) {
     return TableContainer(
-      table(thead(headers.map((header) => th({ scope: "col" }, header))), () =>
-        loading.val
-          ? TableSkeleton({ columnsSize: 2, rowSize: 1 })
-          : tbody(data.val.map(ListItem))
+      table(
+        thead(headers.map((header) => th({ scope: "col" }, header))),
+        () => {
+          if (loading.val) {
+            return TableSkeleton({ columnsSize: 2, rowSize: 1 });
+          } else if (data.val.length) {
+            return tbody(data.val.map(ListItem));
+          }
+          return NoOrg();
+        }
       )
     );
   };
